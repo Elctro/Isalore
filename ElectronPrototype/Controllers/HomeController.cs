1190bd4572adac0e07e3d52d6754c70817eb676e
Unknown <jan.mikulasek@solarwinds.com>
2018-06-12 06:58:30 -0700

    Electron prototype

---
+++ ElectronPrototype/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronPrototype.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HybridSupport.IsElectronActive)
+                Electron.IpcMain.On("select-directory", async args =>
+                {
+                    var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                    var options = new OpenDialogOptions
+                    {
+                        Properties = new[]
+                        {
+                            OpenDialogProperty.openFile
+                        }
+                    };
+
+                    object[] files = await Electron.Dialog.ShowOpenDialogAsync(mainWindow, options);
+                    foreach (var file in files)
+                        if (file is string path)
+                            Process.Start("notepad.exe", path);
+                    Electron.IpcMain.Send(mainWindow, "select-directory-reply", files);
+                });
+            return View();
+        }
+    }
+}
---
+++ ElectronPrototype/Startup.cs
@@ -0,0 +1,60 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ElectronPrototype
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            if (HybridSupport.IsElectronActive) ElectronBootstrap();
+        }
+
+        public async void ElectronBootstrap()
+        {
+            var browserWindow = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
+            {
+                Width = 1152,
+                Height = 864,
+                Show = false
+            });
+
+            browserWindow.OnReadyToShow += () => browserWindow.Show();
+            browserWindow.SetTitle("Electron.NET Prototype");
+        }
+    }
+}
